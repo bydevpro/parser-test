053ad27c73806e332314c0c91abdef6db1b5e2f8
bydevpro <125540679+bydevpro@users.noreply.github.com>
2023-03-16 08:49:30 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', '\App\Http\Controllers\JsonParserController@index');
+Route::get('/result-api', '\App\Http\Controllers\JsonParserController@index');
 
